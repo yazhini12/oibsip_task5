16efc10d5d6854fc7977284930cfce305b90aaf8
yazhini12 <yazhini12503@gmail.com>
2023-12-15 22:32:56 +0530

    StopWatch

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "stop watch"
+include(":app")
+ 
